85d4fb3a15f9df96402f02373abe188f9d978c09
naichilab <naichilab@live.jp>
2017-04-27 22:52:15 +0900

    init

---
+++ Assets/naichilab/unityroom-tweet/TweetSample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace naichilab
+{
+	public class TweetSample : MonoBehaviour
+	{
+		public void Tweet (string text)
+		{
+			naichilab.UnityRoomTweet.Tweet ("ツイートサンプルです。");
+		}
+
+		public void TweetWithHashtag ()
+		{
+			naichilab.UnityRoomTweet.Tweet ("ツイートサンプルです。", "unityroom");
+		}
+
+		public void TweetWithHashtags ()
+		{
+			naichilab.UnityRoomTweet.Tweet ("ツイートサンプルです。", "unityroom", "unity1week");
+		}
+
+	}
+}
---
+++ Assets/naichilab/unityroom-tweet/UnityRoomTweet.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Linq;
+using UnityEngine;
+
+namespace naichilab
+{
+	public static class UnityRoomTweet
+	{
+		/// <summary>
+		/// ツイートします。
+		/// </summary>
+		/// <param name="text">本文</param>
+		/// <param name="hashtag">ハッシュタグ(#は不要) 複数指定可</param>
+		public static void Tweet (string text, params string[] hashtags)
+		{
+			const string SHAREURL = "http://twitter.com/share?";
+
+			var sb = new StringBuilder ();
+			sb.Append (SHAREURL);
+			sb.Append ("original_referer=");
+			sb.Append ("&text=" + WWW.EscapeURL (text));
+			if (hashtags.Any ()) {
+				sb.Append ("&hashtags=" + WWW.EscapeURL (string.Join (",", hashtags)));
+			}
+
+			if (Application.platform == RuntimePlatform.WebGLPlayer) {
+				Application.ExternalEval ("var F = 0;if (screen.height > 500) {F = Math.round((screen.height / 2) - (250));}window.open('" + sb.ToString () + "','intent','left='+Math.round((screen.width/2)-(250))+',top='+F+',width=500,height=260,personalbar=no,toolbar=no,resizable=no,scrollbars=yes');");
+			} else {
+				Debug.Log ("WebGL以外では実行できません。");
+				Debug.Log (sb.ToString ());
+			}
+		}
+	}
+}
